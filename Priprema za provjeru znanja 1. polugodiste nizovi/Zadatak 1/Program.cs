71fd7a1cc0cb606ceea00d311fab66bdd3621f57
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-01-22 19:47:02 +0100

    Add files via upload

---
+++ Priprema za provjeru znanja 1. polugodiste nizovi/Zadatak 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] parni = new int[10];
+            int[] neparni = new int[10];
+
+            int brojevi, p = 0, n = 0;
+
+            do
+            {
+                Console.WriteLine("Unesite broj: ");
+                brojevi = Convert.ToInt32(Console.ReadLine());
+
+                if (brojevi % 2 == 0)
+                {
+                    parni[p++] = brojevi;
+                }
+
+                else
+                {
+                    neparni[n++] = brojevi;
+                }
+
+            } while (brojevi != 0 && p < 10 && n < 10);
+
+            Console.WriteLine("Parni: ");
+
+            for (int i = 0; i < p; i++)
+            {
+                Console.WriteLine(parni[i]);
+            }
+
+            Console.WriteLine("Neparni: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(neparni[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Priprema za provjeru znanja 1. polugodiste nizovi/Zadatak 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz1 = { 1, 5, 7, 2, 8, 1, 3 };
+            int[] niz2 = { 2, 5, 7, 8, 8 };
+            int manji, broj = 0;
+
+            if (niz1.Length < niz2.Length)
+            {
+                manji = niz1.Length;
+            }
+            else
+            {
+                manji = niz2.Length;
+            }
+
+            for (int i = 0; i < manji; i++)
+            {
+                if (niz1[i] == niz2[i])
+                {
+                    broj++;
+                }
+            }
+
+            Console.WriteLine("Broj istih vrijednosti na istim indeksima je " + broj);
+
+            Console.ReadKey();
+        }
+    }
+}
